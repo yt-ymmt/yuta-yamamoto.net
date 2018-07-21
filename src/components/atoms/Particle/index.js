5eb6d5e2f89079afa5b7042826e42639eb3a63e5
Yuta_Yamamoto <yamamoto@yamamoto.local>
2018-07-21 23:25:28 +0900

    add Particles.js

---
+++ src/components/atoms/Particle/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const Particle = ({ ...props }) => (
+    <Particles
+        params={{
+            particles: {
+                line_linked: {
+                    shadow: {
+                        enable: true,
+                        color: '#3CA9D1',
+                        blur: 5
+                    }
+                }
+            },
+            resize: true
+        }}
+        style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#2E324F'
+        }}
+        {...props}
+    />
+);
+export default Particle;
---
+++ src/components/atoms/Particle/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Particle from './index';
+
+storiesOf('Atoms/Particle', module).add('default', () => <Particle />);
