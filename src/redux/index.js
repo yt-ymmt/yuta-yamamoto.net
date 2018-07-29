8a5ba7bf38c9bd428492e6891b3010e01de15547
Yuta_Yamamoto <yamamoto@yamamoto.local>
2018-07-29 15:01:10 +0900

    delete counter reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
-import counter from './reducers/counter';
 import naviItems from './reducers/naviItems';
 import snsItems from './reducers/snsItems';
 import profileItems from './reducers/profileItems';
 
 export const reducer = combineReducers({
-    counter,
     naviItems,
     snsItems,
     profileItems
