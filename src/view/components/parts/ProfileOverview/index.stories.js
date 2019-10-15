b27528545657bf74aef7f8ec94911f4a9aa5b997
Yuta Yamamoto <ftbae1@gmail.com>
2019-10-15 11:58:54 +0900

    delete unused files

--- src/view/components/parts/ProfileOverview/index.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProfileOverview from './index';
-
-import { profileItems } from '../../../mock/data';
-
-storiesOf('Parts/ProfileOverview', module).add('default', () => (
-    <ProfileOverview profileItems={profileItems} />
-));
