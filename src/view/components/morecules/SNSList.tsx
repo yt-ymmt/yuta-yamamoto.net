558b2bf6e5212906e82e2fb48ec07199913bca8c
Yuta Yamamoto <ftbae1@gmail.com>
2019-10-24 09:57:16 +0900

    add icons

--- src/view/components/morecules/SNSList.tsx
+++ src/view/components/morecules/SNSList.tsx
@@ -1,5 +1,5 @@
 import { NextComponentType } from 'next';
-import { Typography } from '@material-ui/core';
+import { Typography, Avatar } from '@material-ui/core';
 import styled from 'styled-components';
 
 type Props = {
@@ -8,13 +8,16 @@ type Props = {
 
 const SNSList: NextComponentType<{}, {}, Props> = ({ className }) => (
     <Root className={className}>
+        <Avatar src="/icons/github.svg" />
+        <Avatar src="/icons/twitter.svg" />
+        <Avatar src="/icons/qiita.svg" />
+        <Avatar src="/icons/dev-dot-to.svg" />
+        <Avatar src="/icons/hatenablog.svg" />
         <Typography variant="body2">Github</Typography>
         <Typography variant="body2">Twitter</Typography>
         <Typography variant="body2">Qiita</Typography>
         <Typography variant="body2">dev.to</Typography>
         <Typography variant="body2">Hatena blog</Typography>
-        <Typography variant="body2">Wantedly</Typography>
-        <Typography variant="body2">Lapras</Typography>
     </Root>
 );
 
